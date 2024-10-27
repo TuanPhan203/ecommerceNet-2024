d355335b74531625a3b76052ea339f611e1e3921
TuanPhan203 <phananhtuan1145@gmail.com>
2024-10-27 09:29:29 +0700

    End build session 1

---
+++ sellTicket/Infrastructure/Data/TheaterContext.cs
@@ -0,0 +1,16 @@
+using System;
+using Core.Entities;
+using Infrastructure.config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class TheaterContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfigration).Assembly);
+    }
+}
